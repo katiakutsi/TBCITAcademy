3d2b0cf5224e54308fadf693e5696aee0212e135
katiakutsi <59748382+katiakutsi@users.noreply.github.com>
2020-04-27 01:11:38 +0400

    Add files via upload

---
+++ task_8.1/task_8.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task_8.1
+//
+//  Created by katia kutsi on 4/25/20.
+//  Copyright © 2020 TBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
